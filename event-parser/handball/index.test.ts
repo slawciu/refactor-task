f8b522d67904a28c3d235f67d8f83e3f2f90c5c7
Sławek Rudawski <slawomir.rudawski@gmail.com>
2023-06-06 10:16:42 +0200

    Added tests for handball match

---
+++ event-parser/handball/index.test.ts
@@ -0,0 +1,26 @@
+import { EventParser } from "../";
+
+describe('when handball match provided', () => {
+  const handballMatch = {
+    sport: 'volleyball',
+    participant1: 'Germany',
+    participant2: 'France',
+    score: '3:0,25:23,25:19,25:21',
+  };
+
+  it('should format score', () => {
+    const parser = new EventParser();
+
+    const score = parser.formatScore(handballMatch);
+
+    expect(score).toBe("Main score: 3:0 (set1 25:23, set2 25:19, set3 25:21)");
+  });
+
+  it('should make event name', () => {
+    const parser = new EventParser();
+
+    const eventName = parser.makeEventName(handballMatch);
+
+    expect(eventName).toBe("Germany - France");
+  });
+});
