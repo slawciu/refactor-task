d6170a06a2ef710828770fae4a8abdc4b29f39c1
Sławek Rudawski <slawomir.rudawski@gmail.com>
2023-06-06 12:12:57 +0200

    Removed throwing exceptions since there is no error handling yet

--- event-parsers-factory/score-formatters/sets-score/index.ts
+++ event-parsers-factory/score-formatters/sets-score/index.ts
@@ -4,8 +4,6 @@ import { IScoreFormatter } from "..";
 export class ScoreIncludingSetsFormatter implements IScoreFormatter {
   format(score: Score): string {
     let scores = (score as string).split(',');
-    if (!scores || scores.length < 4)
-      throw new Error("Invalid score format");
 
     let main = scores[0];
     let sets = scores.slice(1);
