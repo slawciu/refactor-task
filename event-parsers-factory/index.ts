1f31b5ca093088098fce10728caabb27325def45
Sławek Rudawski <slawomir.rudawski@gmail.com>
2023-06-06 11:25:16 +0200

    Implement factory for all known sport disciplines

--- event-parsers-factory/index.ts
+++ event-parsers-factory/index.ts
@@ -6,6 +6,31 @@ export class EventParserFactory {
     const builder = new EventParserBuilder();
 
     switch (discipline) {
+      case 'basketball':
+        return builder
+          .withScoreArrayFormatter()
+          .withDashedParticipantsNameMaker()
+          .build();
+      case 'soccer':
+        return builder
+          .withDashedParticipantsNameMaker()
+          .withStraightScoreFormatter()
+          .build();
+      case 'tennis':
+        return builder
+          .withScoreIncludingSetsFormatter()
+          .withVersusNameMaker()
+          .build();
+      case 'volleyball':
+        return builder
+          .withDashedParticipantsNameMaker()
+          .withScoreIncludingSetsFormatter()
+          .build();
+      case 'handball':
+        return builder
+          .withStraightScoreFormatter()
+          .withVersusNameMaker()
+          .build();
       default:
         return builder.build();
     }
