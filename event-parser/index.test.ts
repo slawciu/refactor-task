8a47128e826acd94fb19be713a2f19ca534b1a35
Sławek Rudawski <slawomir.rudawski@gmail.com>
2023-06-06 10:08:57 +0200

    Added tests for soccer match

--- event-parser/index.test.ts
+++ event-parser/index.test.ts
@@ -24,3 +24,29 @@ describe('when volleyball match provided', () => {
     expect(eventName).toBe("Germany - France");
   });
 });
+
+
+describe('when soccer match provided', () => {
+  const soccerMatch = {
+    sport: 'soccer',
+    participant1: 'Chelsea',
+    participant2: 'Arsenal',
+    score: '2:1',
+  };
+
+  it('should format score', () => {
+    const parser = new EventParser();
+
+    const score = parser.formatScore(soccerMatch);
+
+    expect(score).toBe("2:1");
+  });
+
+  it('should make event name', () => {
+    const parser = new EventParser();
+
+    const eventName = parser.makeEventName(soccerMatch);
+
+    expect(eventName).toBe("Chelsea - Arsenal");
+  });
+});
