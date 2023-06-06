cf9f137d9000019cddda775ac68802adc64fc85a
Sławek Rudawski <slawomir.rudawski@gmail.com>
2023-06-06 11:07:24 +0200

    Created interfaces: IScoreFormatter and INameMaker

---
+++ event-parsers-factory/name-makers/index.ts
@@ -0,0 +1,3 @@
+export interface INameMaker {
+  makeName(participant1: string, participant2: string): string;
+}
---
+++ event-parsers-factory/score-formatters/index.ts
@@ -0,0 +1,3 @@
+export interface IScoreFormatter {
+  format(score: Score): string;
+}
