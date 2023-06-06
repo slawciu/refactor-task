c3381a7287cb64b5742a725a82c53a6e0835bb7e
Sławek Rudawski <slawomir.rudawski@gmail.com>
2023-06-06 10:11:22 +0200

    Split test fixture by discipline

--- event-parser/volleyball/index.test.ts
+++ event-parser/volleyball/index.test.ts
@@ -1,4 +1,4 @@
-import { EventParser } from ".";
+import { EventParser } from "../";
 
 describe('when volleyball match provided', () => {
   const volleyballMatch = {
@@ -22,31 +22,5 @@ describe('when volleyball match provided', () => {
     const eventName = parser.makeEventName(volleyballMatch);
 
     expect(eventName).toBe("Germany - France");
-  });
-});
-
-
-describe('when soccer match provided', () => {
-  const soccerMatch = {
-    sport: 'soccer',
-    participant1: 'Chelsea',
-    participant2: 'Arsenal',
-    score: '2:1',
-  };
-
-  it('should format score', () => {
-    const parser = new EventParser();
-
-    const score = parser.formatScore(soccerMatch);
-
-    expect(score).toBe("2:1");
-  });
-
-  it('should make event name', () => {
-    const parser = new EventParser();
-
-    const eventName = parser.makeEventName(soccerMatch);
-
-    expect(eventName).toBe("Chelsea - Arsenal");
   });
 });
