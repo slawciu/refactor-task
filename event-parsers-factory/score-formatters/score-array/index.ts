a57668f0732d2668a98ce7d48580a9da2110bdae
Sławek Rudawski <slawomir.rudawski@gmail.com>
2023-06-06 12:02:14 +0200

    Changed the way how array score is being built

--- event-parsers-factory/score-formatters/score-array/index.ts
+++ event-parsers-factory/score-formatters/score-array/index.ts
@@ -3,6 +3,6 @@ import { IScoreFormatter } from "..";
 
 export class ScoreArrayFormatter implements IScoreFormatter {
   format(score: Score): string {
-    return score[0][0] + ',' + score[0][1] + ',' + score[1][0] + ',' + score[1][1];
+    return `${score[0][0]},${score[0][1]},${score[1][0]},${score[1][1]}`;
   }
-}
+};
